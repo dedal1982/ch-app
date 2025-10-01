70994d42ee3e7651a9294540b26bde7162b033d3
Алексей <d44.a1@yandex.ru>
2025-10-01 15:17:17 +0300

    update Footer.css and FooterRrequisites.jsx

--- src/components/Footer/FooterRrequisites.jsx
+++ src/components/Footer/FooterRrequisites.jsx
@@ -1,29 +1,53 @@
+import { useState } from "react";
+import "./Footer.css";
+
 function FooterRrequisites() {
+  const [active, setActive] = useState(false);
+
   return (
-    <>
-      <div id="requisites" className="footer__top">
-        <div className="footer__contacts">
-          <ul className="footer__contacts-list requisites-hover">
-            <li>Наименование юридического лица: ООО «Честный Эйб»</li>
-            <li>Юридический адрес: 143980, Московская обл.,</li>
-            <li>г. Балашиха, ул. Октябрьская (Железнодорожный мкр.),</li>
-            <li>д. 33, Бдок А, помещение V-13, комната 1</li>
-          </ul>
-          <ul className="footer__contacts-list requisites-hover">
-            <li>ОГРН: 1195081055575</li>
-            <li>ИНН: 5012100464</li>
-            <li>КПП: 501201001</li>
-          </ul>
-        </div>
-        <ul className="footer__contacts-list bank-list requisites-hover">
-          <li>Банковские реквизиты:</li>
-          <li>Банк: АО «АЛЬФА-БАНК»</li>
-          <li>Расчетный счет: 40702810902630003334</li>
-          <li>Корреспондентский счет: 30101810200000000593</li>
-          <li>БИК: 044525593</li>
+    <div id="requisites" className="footer__top">
+      {/* Контакты с требованиями */}
+      <div className="footer__contacts">
+        <ul
+          className={`footer__contacts-list requisites-hover ${
+            active ? "active" : ""
+          }`}
+          onMouseEnter={() => setActive(true)}
+          onMouseLeave={() => setActive(false)}
+        >
+          <li>Наименование юридического лица: ООО &laquo;Честный Эйб&raquo;</li>
+          <li>Юридический адрес: 143980, Московская обл.,</li>
+          <li>г. Балашиха, ул. Октябрьская (Железнодорожный мкр.),</li>
+          <li>д. 33, Бдок А, помещение V-13, комната 1</li>
+        </ul>
+        <ul
+          className={`footer__contacts-list requisites-hover ${
+            active ? "active" : ""
+          }`}
+          onMouseEnter={() => setActive(true)}
+          onMouseLeave={() => setActive(false)}
+        >
+          <li>ОГРН: 1195081055575</li>
+          <li>ИНН: 5012100464</li>
+          <li>КПП: 501201001</li>
         </ul>
       </div>
-    </>
+
+      {/* Банковские реквизиты */}
+      <ul
+        className={`footer__contacts-list bank-list requisites-hover ${
+          active ? "active" : ""
+        }`}
+        onMouseEnter={() => setActive(true)}
+        onMouseLeave={() => setActive(false)}
+      >
+        <li>Банковские реквизиты:</li>
+        <li>Банк: АО &laquo;АЛЬФА-БАНК&raquo;</li>
+        <li>Расчетный счет: 40702810902630003334</li>
+        <li>Корреспондентский счет: 30101810200000000593</li>
+        <li>БИК: 044525593</li>
+      </ul>
+    </div>
   );
 }
 
