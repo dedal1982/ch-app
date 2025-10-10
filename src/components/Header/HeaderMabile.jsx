47d8b4a1008d018ae9f6352ebbb88ed39cc70600
Алексей <d44.a1@yandex.ru>
2025-10-10 15:29:16 +0300

    update HeaderMabile.jsx and Popup.css

--- src/components/Header/HeaderMabile.jsx
+++ src/components/Header/HeaderMabile.jsx
@@ -1,36 +1,112 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import ContactsPopup from "../Popups/ContactsPopup";
+import useModal from "../../hooks/useModal";
+import { lazy, Suspense } from "react";
+import Preloader from "../Preloader/Preloader";
+const LazyOverlay = lazy(() => import("../Overlay/Overlay"));
+
 function HeaderMabile() {
+  const [contactsText, setContactsText] = useState("Контакты");
+
+  const handleMouseOver = (name) => {
+    setContactsText(name);
+  };
+
+  const handleMouseOut = () => {
+    setContactsText("Контакты");
+  };
+
+  const handleContactsText = (name) => {
+    setContactsText(name);
+  };
+
+  const { isOpen, open, close } = useModal(false);
+
+  const handleOpen = () => {
+    open();
+  };
+
+  const handleClose = () => {
+    close();
+  };
   return (
-    <div className="header__contacts header__contacts-mobile">
-      <div id="header-contacts" className="header__contacts-name">
-        Контакты
+    <>
+      <div
+        className="header__contacts header__contacts-mobile"
+        tabIndex={0}
+        onClick={handleOpen}
+        aria-label="Кими"
+      >
+        <div id="header-contacts" className="header__contacts-name">
+          {contactsText}
+        </div>
+        <ul className="header__contacts-phones">
+          <li>
+            <span />
+            <a
+              href="tel:+79165718989"
+              data-name="Сайты"
+              onMouseOver={() => handleMouseOver("Сайты")}
+              onMouseOut={handleMouseOut}
+              onFocus={() => handleContactsText("Сайты")}
+              onBlur={handleMouseOut}
+            >
+              +7 (916) 571-89-89
+            </a>
+          </li>
+          <li>
+            <span />
+            <a
+              href="tel:+79255718989"
+              data-name="Игры"
+              onMouseOver={() => handleMouseOver("Игры")}
+              onMouseOut={handleMouseOut}
+              onFocus={() => handleContactsText("Игры")}
+              onBlur={handleMouseOut}
+            >
+              +7 (925) 571-89-89
+            </a>
+          </li>
+          <li>
+            <span />
+            <a
+              href="tel:+79645718989"
+              data-name="Тестирование"
+              onMouseOver={() => handleMouseOver("Тестирование")}
+              onMouseOut={handleMouseOut}
+              onFocus={() => handleContactsText("Тестирование")}
+              onBlur={handleMouseOut}
+            >
+              +7 (964) 571-89-89
+            </a>
+          </li>
+          <li>
+            <span />
+            <a
+              href="tel:+79855718989"
+              data-name="Симбиоз"
+              onMouseOver={() => handleMouseOver("Симбиоз")}
+              onMouseOut={handleMouseOut}
+              onFocus={() => handleContactsText("Симбиоз")}
+              onBlur={handleMouseOut}
+            >
+              +7 (985) 571-89-89
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul className="header__contacts-phones">
-        <li>
-          <span />
-          <a href="tel:+79165718989" data-name="Сайты">
-            +7 (916) 571-89-89
-          </a>
-        </li>
-        <li>
-          <span />
-          <a href="tel:+79255718989" data-name="Игры">
-            +7 (925) 571-89-89
-          </a>
-        </li>
-        <li>
-          <span />
-          <a href="tel:+79645718989" data-name="Тестирование">
-            +7 (964) 571-89-89
-          </a>
-        </li>
-        <li>
-          <span />
-          <a href="tel:+79855718989" data-name="Симбиоз">
-            +7 (985) 571-89-89
-          </a>
-        </li>
-      </ul>
-    </div>
+      {isOpen && (
+        <Suspense fallback={<Preloader />}>
+          {createPortal(
+            <LazyOverlay onClose={handleClose}>
+              <ContactsPopup />
+            </LazyOverlay>,
+            document.body
+          )}
+        </Suspense>
+      )}
+    </>
   );
 }
 
